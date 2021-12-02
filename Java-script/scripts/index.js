743b7d91fce475993d4bc1f92c450e71fb4fba2c
avinash <avinashgurugubelli@gmail.com>
2021-12-02 09:14:39 +0530

    json examples added.

--- Java-script/scripts/index.js
+++ Java-script/scripts/index.js
@@ -4,6 +4,7 @@
 
 // Number datatype variable
 let a = 400;
+
 // console.log -> will print to the browser console.
 console.log(a);
 // typeof -> to check the data type of the variable.
@@ -63,24 +64,125 @@ console.log(city);
  * 
  * syntax: let variable_name = {"key": "value"}
  * - Key should be unique
+ * 
+ * refer: https://www.w3schools.com/js/js_json_intro.asp
+ * 
  */
 
-let car = {
+let car1 = {
     'brand': 'Sokoda',
     'name': 'Rapid',
     'year': 2021,
     'color': 'red'
 };
 
-// let car2 = {
-//     'brand': 'Honda',
-//     'name': 'City',
-//     'year': 2020,
-//     'color': 'white'
-// };
+console.log("Type of car 1: " + typeof car1);
 
-console.log(car);
+let car2 = {
+    'brand': 'Honda',
+    'name': 'City',
+    'year': 2020,
+    'color': 'white'
+};
+
+// Nested object
+let person = {
+    firstName: 'Jack',
+    lastName: 'Kollar',
+    age: 50,
+    address: {
+        permnantAddress: {
+            addressLine1: '',
+            addressLine2: '',
+            city: 'Hyd',
+            state: 'TS'
+        },
+        currentAddress: {
+            addressLine1: '',
+            addressLine2: '',
+            city: 'Mumbai',
+            state: 'Maharastra'
+        }
+    },
+    skills: [
+        {
+            skillName: 'Javascript',
+            experience: 5
+        },
+        {
+            skillName: 'Web development',
+            experience: 5
+        }
+    ],
+    phoneNumbers: [152255, 44521]
+};
+
+console.log(car1);
 // accessing a given property
-console.log('Car brand: ' + car.brand);
-console.log('Car color: ' + car.color);
-console.log('Car year: ' + car['year']);
+console.log('Car brand: ' + car1.brand);
+console.log('Car color: ' + car1.color);
+console.log('Car year: ' + car1['year']);
+
+// Accessing nested object value
+console.log('Perminant address city: ' + person.address.permnantAddress.city);
+console.log("Person first skill: " + person.skills[0].skillName);
+// Or
+console.log('current address city: ' + person['address']['currentAddress']['city']);
+
+
+/**
+ * Array: is a special variable which can hold multiple values.
+ * example: let names = ["Jack", "Jim", "Jhon"]
+ * Accessing elements in array:
+ * console.log(names[0]);
+ * 
+ */
+// Declaration
+let names = ["Jack", "Jim", "Jhon"];
+let cars = [car1, car2];
+
+//  Accessing elements in array
+console.log("name at index 0: " + names[0]);
+console.log("name at index 1: " + names[1]);
+console.log("name at index 2: " + names[2]);
+// JSON.stringify -> will convert object as strting
+console.log("car details at index 0: " + JSON.stringify(cars[0]));
+console.log("car details at index 1: " + JSON.stringify(cars[1]));
+
+console.log("cars list: " + JSON.stringify(cars));
+
+console.log("Person details: " + JSON.stringify(person));
+
+console.log("Person Address details: " + JSON.stringify(person.address));
+
+// Updating an object
+// Example (updating property directly): Update person age.
+person.age = 56;
+
+// Example (updating property inside array): update person skill - 1
+person.skills[0].skillName = "java";
+
+/**
+ * Adding new element to existing JSON object
+ * Example: add new proprty called currentWorkingCompany to person object.
+ */
+person['currentWorkingCompany'] = 'Apple';
+
+// Adding a new proprty to an existing array first element.
+person.skills[0]["rating"] = 5;
+
+// Adding a new property whose vaue is of type obj
+person['workExperience'] = [
+    {
+        company: 'ABX',
+        noOfYears: 2,
+        projectName: 'XYS'
+    },
+    {
+        company: 'Intel',
+        noOfYears: 5,
+        projectName: 'Graphics'
+    }
+];
+
+console.log('Updated person details: ' + JSON.stringify(person));
